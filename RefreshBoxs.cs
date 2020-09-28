15d314ce63374112b6eb7575361ed8bf0d22d3d9
Sharif Ahamed <sharifahamed28.sa@gmail.com>
2020-09-28 20:15:29 +0600

    Refresh

---
+++ RefreshBoxs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CURD_Form_Applicatin_using_OOP
+{
+    partial class Form1
+    {
+        private void refreshBoxs()
+        {
+            customerID = 0;
+            fstnamebox.Clear();
+            lstnamebox.Clear();
+            agebox.Clear();
+            citybox.Clear();
+            idbox.Clear();
+        }
+    }
+}
