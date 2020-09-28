950e8123cbfe53396d5a8d7c3fbffefeb885ceb4
Sharif Ahamed <sharifahamed28.sa@gmail.com>
2020-09-28 20:16:21 +0600

    Get records from database

---
+++ GetRecord.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace CURD_Form_Applicatin_using_OOP
+{
+    partial class Form1
+    {
+        private void getCustomerRecord()
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=LPSHARIF;Initial Catalog=Customer;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("select * from Customer", con);
+            DataTable dt = new DataTable();
+            con.Open();
+
+            SqlDataReader sdr = cmd.ExecuteReader();
+            dt.Load(sdr);
+            con.Close();
+
+            CustomerTable.DataSource = dt;
+        }
+    }
+}
